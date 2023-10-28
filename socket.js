da3ea498acba7e0f1844a06f31ab2f9b5e6c522a
Vanv1k <ivanspecial5@gmail.com>
2023-10-28 23:56:40 +0300

    moving functions

--- socket.js
+++ socket.js
@@ -3,7 +3,7 @@ const socket = io();
 function connectToStation() {
     console.log('Connecting to station...');
     const host = document.getElementById('host').value;
-    socket.emit('connect to station', host);
+    socket.emit('connectToStation', host);
 }
 
 function disconnectFromStation() {
@@ -11,3 +11,34 @@ function disconnectFromStation() {
     socket.disconnect();
     console.log('Disconnected!');
 }
+
+function moveForward() {
+    console.log('move Forward...');
+    socket.emit('moveForward', );
+}
+
+function moveRight() {
+    console.log('move Right...');
+    socket.emit('moveRight', );
+}
+
+function moveLeft() {
+    console.log('move Left...');
+    socket.emit('moveLeft', );
+}
+
+function moveBack() {
+    console.log('move back...');
+    socket.emit('moveBack', );
+}
+
+function turnRight() {
+    console.log('turn right...');
+    socket.emit('turnRight', );
+}
+
+function turnLeft() {
+    console.log('turn left...');
+    socket.emit('turnLeft', );
+}
+
